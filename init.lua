bbffbcda3790bb2c15b1e99848310102dac527d4
EntSteve <stenterline@gmail.com>
2025-12-23 14:57:42 -0500

    add debug option

--- init.lua
+++ init.lua
@@ -45,7 +45,10 @@ end)
 
 -- Indentation
 vim.o.breakindent = true
-
+vim.o.tabstop = 4 -- Number of spaces a tab counts for
+vim.o.shiftwidth = 4 -- Number of spaces for auto-indent
+vim.o.expandtab = true -- Convert tabs to spaces
+vim.o.softtabstop = 4 -- Number of spaces for <Tab> in insert mode
 -- ============================================================================
 -- KEYMAPS
 -- ============================================================================
@@ -439,7 +442,7 @@ require('lazy').setup({
       'folke/lazydev.nvim',
     },
     opts = {
-      keymap = { preset = 'default' },
+      keymap = { preset = 'super-tab' },
       appearance = {
         nerd_font_variant = 'mono',
       },
@@ -498,7 +501,82 @@ require('lazy').setup({
       end
     end,
   },
+  -- ========================================
+  -- Debugging (nvim-dap)
+  -- ========================================
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
 
+      -- Language-specific debuggers
+      'mfussenegger/nvim-dap-python',
+      'leoluz/nvim-dap-go',
+    },
+    keys = function(_, keys)
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      return {
+        { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
+        { '<F1>', dap.step_into, desc = 'Debug: Step Into' },
+        { '<F2>', dap.step_over, desc = 'Debug: Step Over' },
+        { '<F3>', dap.step_out, desc = 'Debug: Step Out' },
+        { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
+        {
+          '<leader>B',
+          function()
+            dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+          end,
+          desc = 'Debug: Set Conditional Breakpoint',
+        },
+        { '<F7>', dapui.toggle, desc = 'Debug: Toggle UI' },
+        unpack(keys),
+      }
+    end,
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      require('mason-nvim-dap').setup {
+        automatic_installation = true,
+        handlers = {},
+        ensure_installed = {
+          'delve', -- Go
+          'debugpy', -- Python
+        },
+      }
+
+      -- Setup DAP UI
+      dapui.setup {
+        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+        controls = {
+          icons = {
+            pause = '⏸',
+            play = '▶',
+            step_into = '⏎',
+            step_over = '⏭',
+            step_out = '⏮',
+            step_back = 'b',
+            run_last = '▶▶',
+            terminate = '⏹',
+            disconnect = '⏏',
+          },
+        },
+      }
+
+      -- Automatically open/close DAP UI
+      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+      -- Language-specific setup
+      require('dap-python').setup 'python'
+      require('dap-go').setup()
+    end,
+  },
   -- ========================================
   -- Treesitter (Syntax Highlighting)
   -- ========================================
